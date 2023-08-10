ca0b4564758735e6feb35b1f38fd2f9f2504019a
lefIngridL <90441834+lefIngridL@users.noreply.github.com>
2023-08-10 14:15:47 +0200

    innit
    ukesoppgave 1

---
+++ ukesoppgave1.js
@@ -0,0 +1,15 @@
+
+
+
+function FargeFryd() {
+var div = document.querySelector("div");
+div.className = "head2";
+
+var temp =document.querySelectorAll(".head2");
+
+for (var i =0; i < temp.length; i++) {
+    temp[i].style.background= "green";
+    temp[i].style.fontSize ="40px";
+}
+}
+
